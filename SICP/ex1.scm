30d0c447cc2a2c59ed1ec727a395f1bfe9619e10
bruce <leilmyxwz@gmail.com>
2014-02-26 01:03:37 -0500

    Arkman function

--- SICP/ex1.scm
+++ SICP/ex1.scm
@@ -37,3 +37,9 @@
       product
       (factor-iter (+ counter 1) (* counter product))))
   (factor-iter 1 1))
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (#t (A (- x 1) (A x (- y 1))))))
